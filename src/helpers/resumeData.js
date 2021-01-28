14a16dcc2e87c3ceddbbc22b61b6f8fa7c7e1742
Michelle Kaplan <kaplan.michelle7@gmail.com>
2021-01-28 11:21:19 -0700

    Add resumeData

---
+++ src/helpers/resumeData.js
@@ -0,0 +1,64 @@
+export const resumeData = {
+  gitHubURL: "https://github.com/michellekaplan7",
+  linkedInURL: "https://www.linkedin.com/in/kaplanmichelle",
+  bioDescription: {
+    paragraph1:
+      "My name is Michelle Kaplan and I am a software engineer! Being a former marketer, I am highly empathetic to the user, understand the user journey and implement human centered design to create intuitive, meaningful user experiences. ",
+    paragraph2:
+      "I have a lot of experience working with cross-functional teams and utilize my strong communication skills to communicate goals and objectives across disciplines.",
+    paragraph3:
+      "When building an application, I am passionate about focusing on test driven development and building robust unit and integration tests. I’ve used these skills to build numerous accessible applications that use JavaScript, HTML, CSS/SASS, React, React Hooks, and more!",
+    paragraph4:
+      "Software engineering brings immense excitement to my life. With the vast amount of information to learn, I am continuously propelled to grow and learn.",
+    paragraph5: "Let's build something together!",
+  },
+  education: [
+    {
+      school: "Turing School of Software & Design",
+      major: "Front End Engineering",
+      time: "January 2020 - August 2020",
+      location: "Denver, CO - Remote",
+    },
+    {
+      school: "Colorado State University",
+      major: "B.S. Business Administration, Marketing",
+      time: "August 2013 - December 2016",
+      location: "Fort Collins, CO",
+    },
+  ],
+  work: [
+    {
+      name: "Nanno",
+      position: "Software Engineer",
+      time: "September 2020 - December 2020",
+      description: [
+        "Modernized a Rails monolithic app into a progressive web app",
+        "Built a React/Redux front-end and integrated it to a new back-end",
+        "Designed and implemented testing strategy for front-end components",
+        "Collaborated with other team members with an agile approach",
+        "Lead and participated in technical design meetings, pair programming sessions and code reviews",
+        "Wrote understandable, testable code with an eye towards maintainability and scalability",
+      ],
+    },
+    {
+      name: "Cochlear Americas",
+      position: "Marketing Specialist",
+      time: "July 2018 - October 2019",
+      description: [
+        "Developed paid Facebook campaigns that resulted in over $80,000 of revenue",
+        "Created and implemented a newsletter strategy, aiming to increase engagement and open rates for 55,000 recipients",
+        "Managed the production of recipient testimonial videos, including editing, approval, publishing, and marketing",
+        "Managed the scholarship program, awarding 8 people $2,000/year, up to 4 years",
+      ],
+    },
+    {
+      name: "Cochlear Americas",
+      position: "Marketing Coordinator",
+      time: "September 2017 - July 2018",
+      description: [
+        "Managed social media, newsletters and blog calendars, including writing Facebook/Twitter posts through Spreadfast",
+        "Collaborated with cross-functional teams to manage the production of both print and digital assets",
+      ],
+    },
+  ],
+};
