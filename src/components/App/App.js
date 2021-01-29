10beb9b6ab294b73770eb0c539999f8597235120
Michelle Kaplan <kaplan.michelle7@gmail.com>
2021-01-28 17:20:17 -0700

    Render Experience component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,7 @@ import Nav from "../Nav/Nav";
 import Header from "../Header/Header";
 import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
 import About from "../About/About";
+import Experience from "../Experience/Experience";
 import useWindowSize from "../../hooks/useWindowSize";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
       <Nav />
       <Header />
       <About />
+      <Experience />
 
       {size.width <= 600 && <HamburgerMenu />}
     </div>
