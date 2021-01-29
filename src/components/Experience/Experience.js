8a56565383e5d5d7dd8d40294435077cf23b847a
Michelle Kaplan <kaplan.michelle7@gmail.com>
2021-01-29 11:44:28 -0700

    Add Experience component

---
+++ src/components/Experience/Experience.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+import WorkCard from "../WorkCard/WorkCard";
+import EducationCard from "../EducationCard/EducationCard";
+
+import Scroll from "react-scroll";
+const Element = Scroll.Element;
+
+const Experience = () => {
+  return (
+    <Element name="experience">
+      <section className="experience">
+        <h2 className="section-heading">{"< Experience />"}</h2>
+        <div className="experience__content">
+          <div className="experience__content--work">
+            <h3 className="experience__content--subheading">Work</h3>
+            <WorkCard />
+          </div>
+
+          <div className="experience__content--education">
+            <h3 className="experience__content--subheading">Education</h3>
+            <EducationCard />
+          </div>
+
+          <div className="experience__content--skills">
+            <h3 className="experience__content--subheading">Skills</h3>
+          </div>
+        </div>
+      </section>
+    </Element>
+  );
+};
+
+export default Experience;
