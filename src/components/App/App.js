7abb018158a27d882d85fb519abe85b2cf3fb87d
Michelle Kaplan <kaplan.michelle7@gmail.com>
2021-02-12 13:16:59 -0700

    Render Projects component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import Header from "../Header/Header";
 import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
 import About from "../About/About";
 import Experience from "../Experience/Experience";
+import Projects from "../Projects/Projects";
 import useWindowSize from "../../hooks/useWindowSize";
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
       <Header />
       <About />
       <Experience />
+      <Projects />
 
       {size.width <= 600 && <HamburgerMenu />}
     </div>
